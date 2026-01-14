91073b824f57d91f8cec6e2abb16a1f7111afe51
IVAOpl <142163081+IVAOpl@users.noreply.github.com>
2026-01-14 23:49:04 +0100

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -2,10 +2,10 @@
 
 export const Env = {
   LOGO_URL: import.meta.env.VITE_LOGO_URL || 'https://assets.br.ivao.aero/logos/tag_white.png',
-  MAIN_SYSTEM_URL: import.meta.env.VITE_MAIN_SYSTEM_URL || 'https://admin.pl.ivao.aero',
+  MAIN_SYSTEM_URL: import.meta.env.VITE_MAIN_SYSTEM_URL || 'https://admin.kronos.pl.ivao.aero',
   DEV: import.meta.env.VITE_DEV === 'false' || 'false',
   IVAO_KRONOS_API_SERVER: import.meta.env.VITE_IVAO_KRONOS_API_HOST || 'https://api.kronos.pl.ivao.aero',
   IVAO_API_SERVER: import.meta.env.VITE_IVAO_API_HOST ||  'https://api.ivao.aero',
   CLIENT_ID: import.meta.env.VITE_CLIENT_ID || 'ee08bd51-7b82-4b42-bd22-5ce4b724666c',
-  CLIENT_URL: import.meta.env.VITE_CLIENT_URL || 'https://admin.pl.ivao.aero/',
+  CLIENT_URL: import.meta.env.VITE_CLIENT_URL || 'https://admin.kronos.pl.ivao.aero/',
 };
