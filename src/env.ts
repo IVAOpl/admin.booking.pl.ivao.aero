7ce07da7ba12a832a59ebffc75abcc655a85778d
IVAOpl <142163081+IVAOpl@users.noreply.github.com>
2026-01-14 21:00:31 +0100

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -2,11 +2,11 @@ const isDev = import.meta.env.MODE === 'development';
 
 export const Env = {
   LOGO_URL: import.meta.env.VITE_LOGO_URL || 'https://assets.br.ivao.aero/logos/tag_white.png',
-  MAIN_SYSTEM_URL: import.meta.env.VITE_MAIN_SYSTEM_URL || 'https://kronos.br.ivao.aero',
+  MAIN_SYSTEM_URL: import.meta.env.VITE_MAIN_SYSTEM_URL || 'https://kronos.pl.ivao.aero',
   DEV: import.meta.env.VITE_DEV === 'true' || isDev,
   IVAO_KRONOS_API_SERVER: import.meta.env.VITE_IVAO_KRONOS_API_HOST || (isDev
     ? 'http://localhost:3003/api'
-    : 'https://api.kronos.br.ivao.aero'),
+    : 'https://api.kronos.pl.ivao.aero'),
   IVAO_API_SERVER: import.meta.env.VITE_IVAO_API_HOST || (isDev
     ? 'http://localhost:3003'
     : ''),
